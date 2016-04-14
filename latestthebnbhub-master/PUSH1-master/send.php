4c2e71d0e13d0aeab23889f09476cefad1089559
jd90 <1418146@rgu.ac.uk>
2016-04-14 16:48:29 +0100

    fg

--- latestthebnbhub-master/PUSH1-master/send.php
+++ latestthebnbhub-master/PUSH1-master/send.php
@@ -199,9 +199,9 @@ $roomid = $_POST['roomid'];
 $conn = new PDO ( "sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 try {
-    $st = $conn->query("INSERT INTO Bookings ([cost], [roomid], [bookingtype], [roomname],[cust_title],[cust_firstname],[cust_surname],[cust_telephone],[cust_email],
+    $st = $conn->query("INSERT INTO Bookings ([bookingid],[cost], [roomid], [bookingtype], [roomname],[cust_title],[cust_firstname],[cust_surname],[cust_telephone],[cust_email],
                                         [cust_address_line1], [cust_address_line2],[cust_postcode],[cust_city])
-                                        VALUES ('".$cost."','".$roomid."','"."1"."','".$roomname."','".$title."','".$firstname."','".$surname."','".$telephone."','".$email."',
+                                        VALUES ('".$bookingid."','".$cost."','".$roomid."','"."1"."','".$roomname."','".$title."','".$firstname."','".$surname."','".$telephone."','".$email."',
                                         '".$address."','".$address2."','".$postcode."','".$city."')");
     {
         $newhtml =
