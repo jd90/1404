ce06564ee2c91e8b11135a601e5b0bdef2992f80
jd90 <1418146@rgu.ac.uk>
2016-04-14 21:37:55 +0100

    fg

--- latestthebnbhub-master/PUSH1-master/searchResultsPage.php
+++ latestthebnbhub-master/PUSH1-master/searchResultsPage.php
@@ -231,13 +231,13 @@ session_start();
                                  AND [bbid] NOT IN (
             SELECT [bbid]
 FROM [Bookings]
-WHERE [bookingenddate]  BETWEEN TRY_PARSE('$datein' AS datetime USING 'en-US') AND TRY_PARSE('$dateout' AS datetime USING 'en-US')
-            AND [bookingstartdate]  BETWEEN TRY_PARSE('$datein' AS datetime USING 'en-US') AND TRY_PARSE('$dateout' AS datetime USING 'en-US')
+WHERE [bookingenddate]  BETWEEN '$datein' AND '$dateout'
+            AND [bookingstartdate]  BETWEEN '$datein' AND '$dateout'
             AND [bbid] NOT IN (
 
                 SELECT [bbid]
 FROM [Bookings]
-WHERE ([bookingstartdate] < TRY_PARSE('$datein' AS datetime USING 'en-US') AND [bookingenddate] > TRY_PARSE('$dateout' AS datetime USING 'en-US') )
+WHERE ([bookingstartdate] < '$datein' AND [bookingenddate] > '$dateout' )
 
 )
         )");
