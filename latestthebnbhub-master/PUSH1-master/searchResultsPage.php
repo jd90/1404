f25b769496b740104788d629f78149704359e00e
jd90 <1418146@rgu.ac.uk>
2016-04-14 20:23:33 +0100

    fg

--- latestthebnbhub-master/PUSH1-master/searchResultsPage.php
+++ latestthebnbhub-master/PUSH1-master/searchResultsPage.php
@@ -227,11 +227,12 @@ session_start();
         $conn = new PDO ( "sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         try{
-            $st = $conn-> query("SELECT * FROM [B&B] WHERE [city] = '$city'
+            $st = $conn-> query("SELECT * FROM [B&B]
+                              WHERE [city] = '$city'
                                  AND [bbid] IN (
             SELECT [Bookings].bbid
 FROM [Bookings]
-WHERE [Bookings].bookingenddate NOT BETWEEN '$datein' AND '$dateout'
+WHERE [Bookings].bookingenddate NOT BETWEEN 'h' AND '$dateout'
             AND [Bookings].bookingstartdate NOT BETWEEN '$datein' AND '$dateout'
             AND [Bookings].bbid NOT IN (
 
