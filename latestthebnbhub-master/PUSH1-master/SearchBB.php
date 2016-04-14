adef8e3c319072985ec54b66e5d59ac81b93d468
jd90 <1418146@rgu.ac.uk>
2016-04-14 21:36:09 +0100

    fg

--- latestthebnbhub-master/PUSH1-master/SearchBB.php
+++ latestthebnbhub-master/PUSH1-master/SearchBB.php
@@ -92,10 +92,10 @@ session_start();
     <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
     <script>
         $(function() {
-            $( "#datein" ).datepicker();
+            $( "#datein" ).datepicker({ dateFormat: 'mm-dd-yyyy' });
         });
         $(function() {
-            $( "#dateout" ).datepicker({ dateFormat: 'yy-mm-dd' });
+            $( "#dateout" ).datepicker({ dateFormat: 'mm-dd-yyyy' });
         });
     </script>
 </head>
