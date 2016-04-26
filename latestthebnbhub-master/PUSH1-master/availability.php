5ffec491584a6424d1d3ad0bbdad6314f2362b58
jd90 <1418146@rgu.ac.uk>
2016-04-26 12:42:13 +0100

    fg

--- latestthebnbhub-master/PUSH1-master/availability.php
+++ latestthebnbhub-master/PUSH1-master/availability.php
@@ -93,11 +93,12 @@ if($_POST['bookingstart'] != null){
 if($_POST['bookingstartcancel'] != null) {
     $bookstart2 = $_POST['bookingstartcancel'];
     $bookend2 = $_POST['bookingendcancel'];
+    $roomid = $_GET['roomid'];
 
     $conn = new PDO ("sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     try {
-        $st1 = "DELETE FROM [bookings]  WHERE ([bookingenddate] = '" . $bookend2 . "' AND [bookingstartdate] = '" .  $bookstart2 . "')";
+        $st1 = "DELETE FROM [bookings]  WHERE ([bookingenddate] = '" . $bookend2 . "' AND [bookingstartdate] = '" .  $bookstart2 . "') AND [roomid]= '" .$roomid . "'";
         $conn->exec($st1);
     } catch (PDOException $e) {
         print"$e";
